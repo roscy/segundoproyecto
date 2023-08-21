bbe3c405446ea9d8418b3776cb1e8661398ddcba
roscio cardenas <cardenasrdv@gmail.com>
2023-08-21 19:25:37 -0300

    entrega del segundo proyecto

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
-function enableDarkMode() {
-    let main_body = document.body;
-    main_body.classList.toggle("dark-mode");
+// Función para cambiar entre los modos claro y oscuro
+document.getElementById('input').addEventListener('change', function () {
+  if (!document.body.classList.contains('dark')) {
+    document.body.classList.add('dark');
+  }
+  else {
+    document.body.classList.remove('dark');
+  }
+});
+
+
+const themeToggle = document.getElementById("theme-toggle");
+if (themeToggle) {
+  themeToggle.addEventListener("click", toggleTheme);
 }
 
-let menuToggle = document.querySelector('menuToggle');
-let header = document.querySelector('header');
-menuToggle.onclick = function(){
-    header.classList.toggle('active');
-}
+const menuItems = document.querySelectorAll('.menu-item')
 
+const menuBtn = document.querySelector('#menu-btn')
 
+menuBtn.addEventListener('click', function () {
+  document.body.classList.toggle('mobile-menu-active')
+})
+
+menuItems.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    const currentItem = document.querySelector('.active')
+    currentItem.classList.remove('active')
+    e.target.classList.add('active')
+  })
+})
